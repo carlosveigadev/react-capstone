d798e19996bd685c9eeff9d1428fd390ec4abae8
wrack <carlosveiga1618@gmail.com>
2021-03-13 17:37:45 -0300

    add addPokemon action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,10 +1,16 @@
 const CHANGE_FILTER = 'CHANGE_FILTER';
+const ADD_POKEMON = 'ADD_POKEMON';
 
 const changeFilter = filter => ({
   type: CHANGE_FILTER,
   payload: filter,
 });
 
+const addPokemon = pokemon => ({
+  type: ADD_POKEMON,
+  payload: pokemon,
+});
+
 export {
-  CHANGE_FILTER, changeFilter,
+  CHANGE_FILTER, changeFilter, ADD_POKEMON, addPokemon,
 };
