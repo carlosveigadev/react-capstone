386b7c284f66e47c87c0329a57ea18f95f4b58d0
wrack <carlosveiga1618@gmail.com>
2021-03-16 13:55:29 -0300

    __test__/request.test: add tests for the api request.

--- src/__test__/request.test.js
+++ src/__test__/request.test.js
@@ -1,15 +1,18 @@
-import { allPokemons, pokemonByCategory } from '../api-requests/request';
+import { allPokemons, pokemonByCategory, fetchPokeData } from '../api-requests/request';
 
-describe('Test the API', () => {
-  it('allPokemons is working and returns an object', () => {
-    allPokemons().then(data => {
-      expect(data).toBe('object');
-    });
+describe('Test the API requests', () => {
+  test('allPokemons is working and returns an object', async () => {
+    const data = await allPokemons();
+    expect(typeof data).toBe('object');
   });
 
-  it('allPokemons is working and returns an object', () => {
-    pokemonByCategory().then(data => {
-      expect(data).toBe('object');
-    });
+  test('pokemonByCategory is working and returns an object', async () => {
+    const data = await pokemonByCategory();
+    expect(typeof data).toBe('object');
+  });
+
+  test('fetchPokeData is working and returns an object', async () => {
+    const data = await fetchPokeData();
+    expect(typeof data).toBe('object');
   });
 });
